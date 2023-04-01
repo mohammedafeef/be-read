d7682311918594c5c0a8299fe6c6dc4b80f712e0
mohammedafeef <mohammedafeef.maf@gmail.com>
2023-04-01 13:31:19 +0530

    wip: integrate service to user book details page

--- src/pages/user/book/[id].tsx
+++ src/pages/user/book/[id].tsx
@@ -1,7 +1,20 @@
 import BookDetailsTemplate from "@templates/user/BookDetails";
+import {GetServerSideProps} from "next";
+import {getBookById} from "@app/services/bookService";
+import {BookDeatailsPageProps} from "@app/types/BookDeatailsPageProps";
 
-export default function BookDetails() {
+export default function BookDetails(props: BookDeatailsPageProps) {
     return (
-        <BookDetailsTemplate/>
+        <BookDetailsTemplate {...props}/>
     )
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const bookId = context.query.id;
+    const book = await getBookById(bookId as string);
+    return {
+        props: {
+            id: book.id,
+            ...book.data(),
+        }
+    }
 }
